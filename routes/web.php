785ad988f835aba724f52fc2906ade19642b9aaa
G2al <g2aluigi@gmail.com>
2025-11-11 12:47:54 +0100

    routes sistemate

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
